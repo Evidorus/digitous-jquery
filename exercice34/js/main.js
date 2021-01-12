dae668d5dc9f383ce0df9d85e5135be306f75bd0
konexio <damir.sagadbekov@live.fr>
2021-01-12 14:43:23 +0100

    v5

---
+++ exercice34/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $.ajax({
+            url: `https://restcountries.eu/rest/v2/name/${$("input").val()}`,
+            success: function(data) {
+                $("#country").html(`${data[0].name}`)
+                $("#capital").html(`${data[0].capital}`)
+            }
+        });
+    })
+})
