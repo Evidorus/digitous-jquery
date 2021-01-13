0450d98446a70a1a0b156ca8c8ad3ab489fe9b68
konexio <damir.sagadbekov@live.fr>
2021-01-13 11:20:07 +0100

    fun

--- exercice21/js/main.js
+++ exercice21/js/main.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
     $(".btn").click(function(){
-        $("#square").animate({
-            width: "500px",
-        })
+            if ($("#square").delay(50).css('width') === "100px") {
+                $("#square").animate({
+                    width: "500px",
+                },100)
+            } else if ($("#square").css('width') === "500px") {
+                $("#square").animate({
+                    width: "100px",
+                },1000)
+            }
+                
     })
+    $(".btnUp").click(function(){
+        $("#square").animate({
+            bottom: "+=50px"
+        })
+    });
+    $(".btnDown").click(function(){
+        $("#square").animate({
+            top: "+=50px"
+        })
+    });
+    $(".btnLeft").click(function(){
+        $("#square").animate({
+            right: "+=50px"
+        })
+    });
+    $(".btnRight").click(function(){
+        $("#square").animate({
+            left: "+=50px"
+        })
+    });
 })
